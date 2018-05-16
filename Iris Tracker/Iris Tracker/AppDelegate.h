211947881dab133177539dda82e345d96f2712a9
babahooja <babahooja@gmail.com>
2018-05-16 20:57:15 +0530

    iris tracker iOS application 🛡

---
+++ Iris Tracker/Iris Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Iris Tracker
+//
+//  Created by Himanshu Ahuja on 16/05/18.
+//  Copyright © 2018 Himanshu Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
