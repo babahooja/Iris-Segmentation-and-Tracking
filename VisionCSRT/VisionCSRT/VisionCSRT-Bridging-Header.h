7f2130020222fe7d97b85c4644ff1e923928d6c6
babahooja <babahooja@gmail.com>
2018-06-14 21:42:11 +0530

    Removed redundant module, find the renovated ver. in Object Tracker

--- VisionCSRT/VisionCSRT/VisionCSRT-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//
-//  Created by Himanshu Ahuja on 6/8/18.
-//  Copyright © 2018 neuro.uno. All rights reserved.
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-//#import "CSRTtrackerOCPP.h"
