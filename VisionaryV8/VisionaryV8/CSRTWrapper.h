4b28b766fa91d1ab6b542dbfc9237dde40f51cf3
babahooja <babahooja@gmail.com>
2018-07-01 22:32:30 -0700

    Adding logging :sound:

---
+++ VisionaryV8/VisionaryV8/CSRTWrapper.h
@@ -0,0 +1,15 @@
+//
+//  CSRTWrapper.h
+//  VisionaryV8
+//
+//  Created by Himanshu Ahuja on 22/06/18.
+//  Copyright © 2018 Himanshu Ahuja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#include<opencv2/core/utility.hpp>
+
+//
+//@interface CSRTWrapper : NSObject
+//
+//@end
---
+++ VisionaryV8/VisionaryV8/CSRTWrapper.mm
@@ -0,0 +1,23 @@
+//
+//  CSRTWrapper.m
+//  VisionaryV8
+//
+//  Created by Himanshu Ahuja on 22/06/18.
+//  Copyright © 2018 Himanshu Ahuja. All rights reserved.
+//
+
+#import "CSRTWrapper.h"
+#import <opencv2/opencv.hpp>
+//#include <opencv2/videoio.hpp>
+//#include <opencv2/highgui.hpp>
+
+using namespace std;
+
+@interface CSRTWrapper: NSObject
+
+@end
+
+
+@implementation CSRTWrapper
+
+@end
